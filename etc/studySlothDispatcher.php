bb65dcb4f8a79860c8059e2835f6e6f82e66bb9b
Athanasios Tsagris <thanostsg@gmail.com>
2024-06-28 11:10:58 +0300

    Update studySlothDispatcher.php
    decouple json response

--- etc/studySlothDispatcher.php
+++ etc/studySlothDispatcher.php
@@ -12,9 +12,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: X-Requested-With");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json;charset=UTF-8");
 
-$result = "";
-$output_class = "";
 $slothData = json_decode(file_get_contents("studySlothData.json"), true);
 
 function searchEntries($key)
@@ -30,25 +29,26 @@ function searchEntries($key)
 
 function checkIfEmpty($term)
 {
-    global $output_class;
-    global $result;
-
     if(!empty(trim($term))){
         return false;
     }
-
-    $output_class = "bad";
-    $result = "Write something!";
+    parseResponse("bad", "Write something!");
     return true;
+}
+
+function parseResponse($class = '', $msg = ''){
+    echo json_encode(['slothResp' => [
+            'class' => $class,
+            'msg'   => $msg
+        ]]);
+    exit();
 }
 
 if (isset($_POST["searchkey"]) && !checkIfEmpty($_POST["searchkey"])) {
     if (searchEntries($_POST["searchkey"])) {
-        $output_class = "bad";
-        $result = "Song Name Found.";
+       return parseResponse("bad", "Song Name Found.");
     } else {
-        $output_class = "positive";
-        $result = "Song Name NOT Found.";
+        return parseResponse("positive", "Song Name NOT Found.");
     }
 }
 
@@ -67,17 +67,8 @@ if (isset($_POST["addentry"]) && !checkIfEmpty($_POST["addentry"])) {
             json_encode($slothData),
             LOCK_EX
         );
-        $output_class = "positive";
-        $result = "Song successfully added!";
+        return parseResponse("positive", "Song successfully added!");
     } else {
-        $output_class = "bad";
-        $result = "Song is already added.";
+        return parseResponse("bad", "Song is already added.");
     }
 }
-echo '<span class="text msg_' .
-    $output_class .
-    '">' .
-    $result .
-    '</span><span class="emoji_msg_' .
-    $output_class .
-    '"></span>';
